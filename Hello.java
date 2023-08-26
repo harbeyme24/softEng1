44db0a5cc5d14d6027c4dc9b47398eec4d4cd855
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-08-26 22:24:22 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, Software Engineering Course!");
+    System.out.println("My name is EDROSO, Harbey M.!");
+  }
+}

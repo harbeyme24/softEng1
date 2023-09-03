1a4ca945b03632085bf0ea46e9f74c31db814103
Harbey <harbeyme24@gmail.com>
2023-09-03 22:00:05 +0800

    added a new file Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello
   {
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is EDROSO, Harbey M.!");
+    System.out.println("Complicated world of SoftDev that is why it pays much!");
   }
 }
